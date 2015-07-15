ed50d535f4cfd3cb4e007f60933325af40f92b57
Robert Schmidtke <ro.schmidtke@gmail.com>
2015-07-15 10:57:30 +0200

    so apparently bytes can only hold numbers, yeah

--- xtreemfs-flink-benchmark/src/main/java/org/xtreemfs/flink/benchmark/TPCH1Benchmark.java
+++ xtreemfs-flink-benchmark/src/main/java/org/xtreemfs/flink/benchmark/TPCH1Benchmark.java
@@ -87,19 +87,19 @@ public class TPCH1Benchmark extends AbstractTPCHBenchmark {
 			// 86400000 milliseconds in a day.
 			final long delta = (new Random().nextInt(61) + 60) * 86400000;
 
-			DataSource<Tuple7<Float, Float, Float, Float, Byte, Byte, String>> lineItems = reader
+			DataSource<Tuple7<Float, Float, Float, Float, String, String, String>> lineItems = reader
 					.types(Float.class, Float.class, Float.class, Float.class,
-							Byte.class, Byte.class, String.class);
+							String.class, String.class, String.class);
 
 			// Filter on date.
-			DataSet<Tuple7<Float, Float, Float, Float, Byte, Byte, String>> filteredLineItems = lineItems
-					.filter(new FilterFunction<Tuple7<Float, Float, Float, Float, Byte, Byte, String>>() {
+			DataSet<Tuple7<Float, Float, Float, Float, String, String, String>> filteredLineItems = lineItems
+					.filter(new FilterFunction<Tuple7<Float, Float, Float, Float, String, String, String>>() {
 
 						private static final long serialVersionUID = -818134663949093125L;
 
 						@Override
 						public boolean filter(
-								Tuple7<Float, Float, Float, Float, Byte, Byte, String> tuple)
+								Tuple7<Float, Float, Float, Float, String, String, String> tuple)
 								throws Exception {
 							Date date = dateParser.parse(tuple.f6);
 							return date.getTime() <= referenceDate - delta;
@@ -107,18 +107,18 @@ public class TPCH1Benchmark extends AbstractTPCHBenchmark {
 					});
 
 			// Map for calculations.
-			DataSet<Tuple9<Byte, Byte, Float, Float, Float, Float, Float, Float, Float>> mappedLineItems = filteredLineItems
-					.map(new MapFunction<Tuple7<Float, Float, Float, Float, Byte, Byte, String>, Tuple9<Byte, Byte, Float, Float, Float, Float, Float, Float, Float>>() {
+			DataSet<Tuple9<String, String, Float, Float, Float, Float, Float, Float, Float>> mappedLineItems = filteredLineItems
+					.map(new MapFunction<Tuple7<Float, Float, Float, Float, String, String, String>, Tuple9<String, String, Float, Float, Float, Float, Float, Float, Float>>() {
 
 						private static final long serialVersionUID = 8021849053433646399L;
 
 						@Override
-						public Tuple9<Byte, Byte, Float, Float, Float, Float, Float, Float, Float> map(
-								Tuple7<Float, Float, Float, Float, Byte, Byte, String> tuple)
+						public Tuple9<String, String, Float, Float, Float, Float, Float, Float, Float> map(
+								Tuple7<Float, Float, Float, Float, String, String, String> tuple)
 								throws Exception {
 							float discountedPrice = tuple.f1
 									* (1.0f - tuple.f2);
-							return new Tuple9<Byte, Byte, Float, Float, Float, Float, Float, Float, Float>(
+							return new Tuple9<String, String, Float, Float, Float, Float, Float, Float, Float>(
 									tuple.f4, tuple.f5, tuple.f0, tuple.f1,
 									discountedPrice, discountedPrice
 											* (1.0f + tuple.f3), tuple.f0,
@@ -126,7 +126,7 @@ public class TPCH1Benchmark extends AbstractTPCHBenchmark {
 						}
 					});
 
-			DataSet<Tuple9<Byte, Byte, Float, Float, Float, Float, Float, Float, Float>> result = mappedLineItems
+			DataSet<Tuple9<String, String, Float, Float, Float, Float, Float, Float, Float>> result = mappedLineItems
 					.groupBy(0, 1).sum(2).andSum(3).andSum(4).andSum(5)
 					.andSum(6).andSum(7).andSum(8)
 					.sortPartition(0, Order.ASCENDING)
@@ -134,13 +134,13 @@ public class TPCH1Benchmark extends AbstractTPCHBenchmark {
 
 			final long count = result.count();
 			result.map(
-					new MapFunction<Tuple9<Byte, Byte, Float, Float, Float, Float, Float, Float, Float>, Tuple9<Byte, Byte, Float, Float, Float, Float, Float, Float, Float>>() {
+					new MapFunction<Tuple9<String, String, Float, Float, Float, Float, Float, Float, Float>, Tuple9<String, String, Float, Float, Float, Float, Float, Float, Float>>() {
 
 						private static final long serialVersionUID = -6596407623350163628L;
 
 						@Override
-						public Tuple9<Byte, Byte, Float, Float, Float, Float, Float, Float, Float> map(
-								Tuple9<Byte, Byte, Float, Float, Float, Float, Float, Float, Float> tuple)
+						public Tuple9<String, String, Float, Float, Float, Float, Float, Float, Float> map(
+								Tuple9<String, String, Float, Float, Float, Float, Float, Float, Float> tuple)
 								throws Exception {
 							tuple.f6 /= count;
 							tuple.f7 /= count;
