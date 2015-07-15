cac06924a64893ecd7758030b3b99e4136997dfe
Schmidtke <bzcschmi@cumulus.zib.de>
2015-07-15 10:02:50 +0200

    went back to provided dependencies

--- xtreemfs-flink-benchmark/src/main/java/org/xtreemfs/flink/benchmark/XtreemFSFlinkBenchmark.java
+++ xtreemfs-flink-benchmark/src/main/java/org/xtreemfs/flink/benchmark/XtreemFSFlinkBenchmark.java
@@ -6,7 +6,7 @@ import java.util.Map.Entry;
 
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
-import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.GnuParser;
 import org.apache.commons.cli.HelpFormatter;
 import org.apache.commons.cli.Option;
 import org.apache.commons.cli.Options;
@@ -38,7 +38,7 @@ public class XtreemFSFlinkBenchmark {
 				"Name of the benchmark to execute. Valid options:"
 						+ benchmarkNames.toString()));
 
-		parser = new DefaultParser();
+		parser = new GnuParser();
 		formatter = new HelpFormatter();
 	}
 
